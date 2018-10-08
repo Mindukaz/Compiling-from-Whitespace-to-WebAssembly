0bd0de5b480481a1521d782a5ba99a253cf1b6dd
Mindukaz <vysniauskas.m@gmail.com>
2018-10-08 11:03:54 +0100

    simple code refactoring. Made it slightly easier to read and debug

--- toy_compiler/main.cpp
+++ toy_compiler/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #define TAB 9
 #define LF 10
 
-char text_chars[100000];
+vector<int> text_chars;
 int char_num = 0;
 vector<string> instructions;
 
@@ -23,69 +23,56 @@ void error(string error)
 	exit(1);
 }
 
-string get_label(int location)
+string get_bits(int location)
 {
+	// does this function need to distinguis between labels and integers?
+	// do the new instruction (slide, copy) use the sign bit at the beginning? 
+	// do i need to worry about it?
+	// output looks like i should be working around it
 	string bits = "";
-	while(int(text_chars[location]) != LF)
+	while(text_chars[location] != LF)
 	{
-		if(int(text_chars[location]) == SPACE) bits += '0';
+		if(text_chars[location] == SPACE) bits += '0';
 		else bits += '1';
 		location++;
 	}
 	return bits;
 }
 
-int get_label_end(int location)
+int get_bits_end(int location)
 {
-	while((int(text_chars[location]) != LF) && (location < char_num)) location++;
+	while((text_chars[location] != LF) && (location < char_num)) location++;
 	return location + 1;
 }
 
 int stack_manipulation(int location)
 {
-	cout << "stack manipulation" << endl;
+	//cout << "stack manipulation" << endl;
 
 	// Push value onto stack
-	if(int(text_chars[location + 1]) == SPACE)
-	{
-		bool positive_value;
-		string bits = "";
-		
+	if(text_chars[location + 1] == SPACE)
+	{	
 		// determine if value is positive or negative
-		if(int(text_chars[location + 2]) == SPACE) positive_value = true;
-		else if(int(text_chars[location + 2]) == TAB) positive_value = false;
-		else if(int(text_chars[location + 2]) == LF)
-		{
-			instructions.push_back("stack push 0");
-			return location + 3;
-		}
-
-		location += 3;
+		bool positive_value;
+		if(text_chars[location + 2] == SPACE) positive_value = true;
+		else if(text_chars[location + 2] == TAB) positive_value = false;
 	
 		// get the bits representing the value
-		while(int(text_chars[location]) != LF)
-		{
-			if(int(text_chars[location]) == SPACE) bits += '0';
-			else if(int(text_chars[location]) == TAB) bits += '1';
-			else break;
-			location++;
-		}
+		string bits = get_bits(location + 3);
+		
 		// get integer value from bits
 		int value;
-		cout << bits << endl;
 		if(bits == "") value = 0; 
 		else value = stoi(bits, nullptr, 2);
 		if(!positive_value) value *=  -1;
-		cout << value << endl;
 
 		instructions.push_back("stack push " + to_string(value));
-		return location + 1;
+		return get_bits_end(location + 3);
 	}
-	// Other stack operation
-	else if(int(text_chars[location + 1]) == LF)
+	// Value manipulation
+	else if(text_chars[location + 1] == LF)
 	{
-		cout << "other stack opeartion" << endl;
-		switch(int(text_chars[location + 2]))
+		switch(text_chars[location + 2])
 		{
 			case SPACE: instructions.push_back("stack duplicate");
 				    break;
@@ -98,16 +85,28 @@ int stack_manipulation(int location)
 		}
 		return location + 3;
 	}
-	else {cout << int(text_chars[location + 1]) << endl; error("Syntax error");}
+	// Deep stack manipulation
+	else if(text_chars[location + 1] == TAB)
+	{
+		switch(text_chars[location + 2])
+		{
+			case SPACE: instructions.push_back("stack copy " + get_bits(location + 3));
+				    break;
+			case LF: instructions.push_back("stack slide " + get_bits(location + 3));
+				 break;
+			default: error("Syntax error");
+		}
+		return get_bits_end(location + 3);
+	}
 }
 
 int arithmetic(int location)
 {
-	cout << "arithmetic" << endl;
+	//cout << "arithmetic" << endl;
 
-	if(int(text_chars[location + 2]) == SPACE)
+	if(text_chars[location + 2] == SPACE)
 	{
-		switch(int(text_chars[location + 3]))
+		switch(text_chars[location + 3])
 		{
 			case SPACE: instructions.push_back("arithmetic add");
 				    break;
@@ -118,9 +117,9 @@ int arithmetic(int location)
 			default: error("Syntax error");
 		}
 	}
-	else if(int(text_chars[location + 2]) == TAB)
+	else if(text_chars[location + 2] == TAB)
 	{
-		switch(int(text_chars[location + 3]))
+		switch(text_chars[location + 3])
 		{
 			case SPACE: instructions.push_back("arithmetic div");
 				    break;
@@ -136,9 +135,9 @@ int arithmetic(int location)
 
 int heap_access(int location)
 {
-	cout << "heap" << endl;
+	//cout << "heap" << endl;
 
-	switch(int(text_chars[location + 2]))
+	switch(text_chars[location + 2])
 	{
 		case SPACE: instructions.push_back("heap store");
 			    break;
@@ -152,44 +151,44 @@ int heap_access(int location)
 
 int flow_control(int location)
 {
-	cout << "flow control" << endl;
+	//cout << "flow control" << endl;
 
-	switch(int(text_chars[location + 1]))
+	switch(text_chars[location + 1])
 	{
-		case SPACE: switch(int(text_chars[location + 2]))
+		case SPACE: switch(text_chars[location + 2])
 			    {
-				    case SPACE: instructions.push_back("flow mark " + get_label(location + 3));
+				    case SPACE: instructions.push_back("flow mark " + get_bits(location + 3));
 						break;
-				    case TAB: instructions.push_back("flow call subroutine " + get_label(location + 3));
+				    case TAB: instructions.push_back("flow call subroutine " + get_bits(location + 3));
 					      break;
-				    case LF: instructions.push_back("flow jump " + get_label(location + 3));
+				    case LF: instructions.push_back("flow jump " + get_bits(location + 3));
 					     break;
 			    }
 			    break;
-		case TAB: switch(int(text_chars[location + 2]))
+		case TAB: switch(text_chars[location + 2])
 			  {
-				  case SPACE: instructions.push_back("flow jump 0 " + get_label(location + 3));
+				  case SPACE: instructions.push_back("flow jump 0 " + get_bits(location + 3));
 					      break;
-				  case TAB: instructions.push_back("flow jump neg " + get_label(location + 3));
+				  case TAB: instructions.push_back("flow jump neg " + get_bits(location + 3));
 					    break;
 				  case LF: instructions.push_back("flow end subroutine");
 					   break;
 			  }
 			  break;
-		case LF: if(int(text_chars[location + 2]) == LF) instructions.push_back("flow end program");
+		case LF: if(text_chars[location + 2] == LF) instructions.push_back("flow end program");
 			 else error("Syntax error");
 			 break;
 	}
-	return get_label_end(location + 3);
+	return get_bits_end(location + 3);
 }
 
 int io(int location)
 {
-	cout << "io" << endl;
+	//cout << "io" << endl;
 
-	if(int(text_chars[location + 2]) == SPACE)
+	if(text_chars[location + 2] == SPACE)
 	{
-		switch(int(text_chars[location + 3]))
+		switch(text_chars[location + 3])
 		{
 			case SPACE: instructions.push_back("io out char");
 				    break;
@@ -198,9 +197,9 @@ int io(int location)
 			default: error("Syntax error");
 		}
 	}
-	else if(int(text_chars[location + 2]) == TAB)
+	else if(text_chars[location + 2] == TAB)
 	{
-		switch(int(text_chars[location + 3]))
+		switch(text_chars[location + 3])
 		{
 			case SPACE: instructions.push_back("io in char");
 				    break;
@@ -219,36 +218,29 @@ void identify_commands()
 {
 	for(int i = 0; i < char_num - 1;)
 	{
-		switch(int(text_chars[i]))
+		switch(text_chars[i])
 		{
-			// Space
 			case SPACE: i = stack_manipulation(i);
 				    break;
 			
-			// Tab
-			case TAB: switch(int(text_chars[i+1]))
+			case TAB: switch(text_chars[i+1])
 				{
-					// Space
 					case SPACE: i = arithmetic(i);
 						    break;
 
-					// Line feed
 					case LF: i = io(i);
 						 break;
 					
-					// Tab
 					case TAB: i = heap_access(i);
 						  break;
 
 					default: error("Synatx error");
 				} 
 				break;
-			
-			// Line feed	
+				
 			case LF: i = flow_control(i);
 				 break;
 		
-			// Syntax error
 			default: error("Syntax error");
 		}
 	}	
@@ -263,19 +255,20 @@ int main(int argc, char* argv[])
 
 	if(file.is_open())
 	{
-		int i = 0;
+		char temp;
 		while(!file.eof())
 		{
-			file.get(text_chars[i++]);
-			char_num++;
+			file.get(temp);
+			if((int(temp) == SPACE) || (int(temp) == TAB) || (int(temp) == LF))
+			{
+				text_chars.push_back(int(temp));
+				char_num++;
+			}
 		}
 	}
 	else error("Could not open file");
 	file.close();
-
-	// clean out any non whitespace characters from array
 	
-
 	// iterate over array to identify commands
 	identify_commands();
 
